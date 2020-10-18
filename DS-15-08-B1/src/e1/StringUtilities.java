be76d5d401cbdade439554d727fdd463494e852a
Esnorlax69 <72927214+Esnorlax69@users.noreply.github.com>
2020-10-18 20:36:53 +0200

    empezando primer ejercicio

---
+++ DS-15-08-B1/src/e1/StringUtilities.java
@@ -0,0 +1,34 @@
+package e1;
+
+public class StringUtilities {
+    private String a;
+    private String b;
+    private String c;
+    /**
+     * Checks if a given string is a valid c of two others . That is , it contains
+     * all characters of the other two and order of all characters in the individual
+     * strings is preserved .
+     * @param a First String to be mixed
+     * @param b Second String to be mixed
+     * @param c Mix of the two other Strings
+     * @return true if the c is valid , false otherwise
+     */
+    public static boolean isValidMix ( String a, String b, String c) {
+
+
+
+        return true;
+    }
+    /**
+     * Generates a random valid mix for two Strings
+     * @param a First String to be mixed
+     * @param b Second String to be mixed
+     * @return A String that is a random valid mix of the other two.
+     */
+    public static String generateRandomValidMix ( String a, String b) {
+
+
+
+        return b;
+    }
+}
