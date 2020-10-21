9853d172846105da0491024ba5dee107a81e8bba
Esnorlax <santiago.rey2@udc.es>
2020-10-21 11:53:04 +0200

    iskeypadValid funciona pasas el test

--- DS-15-08-B1/src/e2/Code.java
+++ DS-15-08-B1/src/e2/Code.java
@@ -3,6 +3,7 @@ package e2;
 import java.util.Arrays;
 
 public class Code {
+
     /**
      * Determines whether a keypad is valid . To do this , it must be a rectangle and
      * the numbers must follow the alphanumeric sequence indicated . If the array
@@ -11,37 +12,87 @@ public class Code {
      * @return true if it is valid , false otherwise .
      */
     public static boolean isKeypadValid (char [][] keypad ) {
-        int numFilas,i=0,k=0,numCol;
-        boolean orden;
+        int numFilas,i=0,k,numCol,orden=0,cont =0,comprletras = 10;
+
         if(keypad == null)
             return false;
-        if(keypad[0][0] != 1)
+        if(keypad[0][0] !='1')
             return false;
         numFilas = keypad.length;
         numCol= keypad[i].length;
 
         //Bucle donde comprobamos que la matric es estrictamente rectangular en caso contrario decuelve falso
         while(i<numFilas){
+            if(keypad[i]==null)
+                return false;
             if(numCol != keypad[i].length)
                 return false;
             i++;
         }
-        if(numCol > 1)
-        orden = keypad[0][1] == 2;
+        if(numCol > 1 && keypad[0][1] =='2'){
+            orden = 0; //Si orden es 0 entonces el orden del keypad es por filas
+        }else if(numFilas > 1 && keypad[1][0] =='2'){
+            orden = 1; //Si orden es 1 entonces el orden del keypad es por columnas
+        }else if(numCol == 1 && numFilas == 1){
+            return true;
+        }
+        //Comprobacion de contenido de orden por filas
+        i=0;
+        if(orden == 0){
+            while(i<numFilas){
+                System.out.println(i);
+                k=0;
+                while(k<numCol){
+                    if(cont < 9 && Character.getNumericValue(keypad[i][k]) != cont+1 ){
+                        return false;
+                    }else if(cont == 9){
+                        if(Character.getNumericValue(keypad[i][k]) != 0)
+                            return false;
+                    }else if(cont > 9 && Character.getNumericValue(keypad[i][k]) != comprletras ){
+                        return false;
+                    }else if(Character.getNumericValue(keypad[i][k]) == comprletras){
+                        if(comprletras < 35){
+                            comprletras++;
+                        }
+                    }
+                    k++;
+                    cont++;
+                }
+                i++;
+            }
+            return true;
+        }
+        //Bucle de comprobacion contenido ordenado por columnas
+        k=0;
+        if(orden == 1){
+            while(k<numCol){
+                i=0;
+                while(i<numFilas){
+                    if(cont < 9 && Character.getNumericValue(keypad[i][k]) != cont+1 ){
+                        return false;
+                    }else if(cont == 9){
+                        if(Character.getNumericValue(keypad[i][k]) != 0)
+                            return false;
+                    }else if(cont > 9 && Character.getNumericValue(keypad[i][k]) != comprletras ){
+                        return false;
+                    }else if(Character.getNumericValue(keypad[i][k]) == comprletras){
+                        System.out.println("Comprobamos la letra");
+                        System.out.println(Character.getNumericValue(keypad[i][k]));
+                        if(comprletras < 35){
+                            comprletras++;
+                        }
+                    }
+                    i++;
+                    cont++;
+                }
+                System.out.println("Cambiamos de fila");
+                k++;
+            }
+            System.out.println("Acabamos de recorrer toda la matriz");
+            return true;
+        }
 
-       for(i=0 ;i<numFilas; i++){
-           for(k=0; k<numCol; k++){
-
-           }
-       }
-
-
-
-
-        System.out.println();
-
-
-        return true;
+        return false;
     }
     /**
      * Checks if an array of movements is valid when obtaining a key on a keypad .
